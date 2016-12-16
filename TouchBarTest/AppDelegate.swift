3363dc4ad0ffbaa2e124349924106096aea7f5b3
Openfield Mobility <pboudon@opnf.fr>
2016-12-16 10:53:28 +0100

    Initial Commit

---
+++ TouchBarTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchBarTest
+//
+//  Created by Openfield Mobility on 16/12/2016.
+//  Copyright © 2016 Openfield Mobility. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchBarTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchBarTest
+//
+//  Created by Openfield Mobility on 16/12/2016.
+//  Copyright © 2016 Openfield Mobility. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
